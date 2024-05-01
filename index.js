a0f81ec7835c1d8e1a508113cfcbeaaa8c2ad432
Пользователь <1nexpl1>
2024-05-02 00:45:12 +0500

    corsOpt

--- index.js
+++ index.js
@@ -11,8 +11,14 @@ const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000
 
+const corsOption = {
+    origin: ['https://api.flowers-pro-vp.ru/'],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}
+
 const app = express()
-app.use(cors())
+app.use(cors(corsOption))
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
