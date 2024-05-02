df5a5a701d45f3f3eea8567a737bc1fb6fde019c
Пользователь <1nexpl1>
2024-05-02 13:52:22 +0500

    access control

--- index.js
+++ index.js
@@ -12,17 +12,18 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000
 
 const corsOption = {
-    origin: ['https://api.flowers-pro-vp.ru/'],
+    origin: '*',
+    allowedHeaders: '*',
     credentials: true,
+    preflightContinue: false,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }
 
 const app = express()
-app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
-app.use('/api', cors(), router)
+app.use('/api', cors(corsOption), router)
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(errorHandler)
