2afba6e1c2aafdca7c45163643781cd8585b1f1b
Пользователь <1nexpl1>
2024-05-02 13:34:39 +0500

    cors update

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ const corsOption = {
 }
 
 const app = express()
-app.use(cors(corsOption))
+app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
-app.use('/api', router)
+app.use('/api', cors(), router)
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(errorHandler)
